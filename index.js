e10d35454edc305900b8420a71f34a815cfaf8c7
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-20 18:44:17 -0400

    refactor

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-/**
+/*!
  * strip-banner <https://github.com/jonschlinkert/strip-banner>
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT license.
+ *
+ * Copyright (c) 2016, Jon Schlinkert.
+ * Released under the MIT license.
  */
 
-module.exports = require('strip-comments').banner;
+'use strict';
+
+var extract = require('extract-banner');
+
+module.exports = function(str, options) {
+  if (!str) return '';
+
+  options = options || {};
+  var banner = extract(str, options.configNames);
+  if (banner === '') {
+    return str;
+  }
+
+  if (options.keepProtected === true) {
+    var m = /[^\s\w]+/.exec(banner);
+    var lead = m ? m[0].trim() : '';
+    if (lead[lead.length - 1] === '!') {
+      return str;
+    }
+  }
+  return str.replace(banner, '');
+};
